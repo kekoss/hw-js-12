358a41731f8e7d10690d7be90ecd100fad2359ee
kekoss <andriy2359@gmail.com>
2023-02-17 19:50:50 +0100

    homework js 12

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// 1. Тому, що в деяких ситуаціях користувач може вводити дані не через клавіатуру
+
+
+const buttons = document.querySelectorAll('.btn');
+let prevActiveButton;
+
+document.addEventListener('keydown', function(event) {
+  const pressedBtn = event.key.toLowerCase();
+  console.log(pressedBtn);
+
+  buttons.forEach(function(button) {
+    const buttonLetter = button.textContent.toLowerCase();
+    
+    if (buttonLetter === pressedBtn) {
+      button.classList.toggle('active');
+      prevActiveButton = button;
+    } else {
+      button.classList.remove('active');
+    }
+  });
+
+  if (prevActiveButton && !prevActiveButton.classList.contains('active')) {
+    prevActiveButton.classList.remove('active');
+  }
+});
+
+
+
+
+
